5ba6adb99994c3cf5c0e9565f6c2cd5ab80ba966
Juanma0186 <juanma0186@gmail.com>
2024-01-17 16:29:35 +0100

    Movido gestor de tareas

--- TEMA_4/PRACTICAS/GestorTareas/README.md
+++
@@ -1,7 +0,0 @@
-# ICONOS BOOTSTRAP
-
-Editar -> brush
-Editar:hover -> brush-fill
-
-Eliminar -> trash
-Eliminar:hover -> trash-fill
--- TEMA_4/PRACTICAS/GestorTareas/src/script/main.js
+++
@@ -1,8 +0,0 @@
-// Añade el modulo de titulo de la carpeta scripts
-import { cambiarTitulo } from './titulo.js';
-
-// Cargamos el modulo cuando se inicia la web
-
-onload = () => {
-  cambiarTitulo();
-}
